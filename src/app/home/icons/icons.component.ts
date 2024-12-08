2d008f40ac64cea0a26a5d54929672d39f494e87
Carlos PC7 <formacionescpc@gmail.com>
2024-12-08 18:42:06 +0100

    Directivas ngfor y ngclass en Icons componente

--- src/app/home/icons/icons.component.ts
+++ src/app/home/icons/icons.component.ts
@@ -8,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './icons.component.css'
 })
 export class IconsComponent {
+  categories = [
+    { name: 'Con vistas', iconClass: 'bi-eye' },
+    { name: 'Céntrico', iconClass: 'bi-building' },
+    { name: 'Espacioso', iconClass: 'bi-arrows-fullscreen' },
+    { name: '24 horas', iconClass: 'bi-clock' },
+  ];
 
+  onCategoryClick(category: any) {
+    console.log('Clicked category:', category);
+  }
 }
