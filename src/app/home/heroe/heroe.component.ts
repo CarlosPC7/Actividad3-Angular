393cb0e8f138030563cf66064e6913465ad8b082
Carlos PC7 <formacionescpc@gmail.com>
2024-12-08 18:21:44 +0100

    Directivas ngmodel, ngclass, ngstyle y ngif en componente Heroe

--- src/app/home/heroe/heroe.component.ts
+++ src/app/home/heroe/heroe.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './heroe.component.css'
 })
 export class HeroeComponent {
+  searchQuery: string = '';
+  searchResults: any[] = [];
 
+  onSearchChange(): void {
+    console.log('Búsqueda en proceso...', this.searchQuery);
+  }
+
+  performSearch(): void {
+    if (this.searchQuery) {
+      console.log('Realizando búsqueda para:', this.searchQuery);
+    } else {
+      console.log('Por favor ingrese una búsqueda');
+    }
+  }
 }
